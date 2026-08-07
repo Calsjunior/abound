9a0011b7a029efc3e627926e5a5aa3ada95ddcda
cal <sakphea05@gmail.com>
2026-08-08 01:11:35 +0700

    feat(todo): add base todo class

---
+++ src/models/Todo.js
@@ -0,0 +1,27 @@
+export class Todo {
+  constructor({
+    title,
+    description = "",
+    dueDate = null,
+    priority = "normal",
+    notes = "",
+    checklist = [],
+  }) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.notes = notes;
+    this.checklist = checklist;
+    this.completed = false;
+  }
+
+  addChecklistItem(text) {
+    this.checklist.push({
+      id: crypto.randomUUID(),
+      text: text,
+      completed: false,
+    });
+  }
+}
