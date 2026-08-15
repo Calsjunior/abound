c177a598768016bd129706c4c98d8244f043831c
cal <sakphea05@gmail.com>
2026-08-15 19:21:39 +0700

    feat(components): add ProjectList class to handle rendering projects

---
+++ src/components/ProjectList.js
@@ -0,0 +1,49 @@
+import { EVENTS } from "../constants/events.js";
+import { createElement } from "../utils/dom.js";
+
+export class ProjectList {
+  constructor(eventBus) {
+    if (typeof eventBus?.publish !== "function") {
+      throw new Error("An eventBus with 'publish' method is required.");
+    }
+
+    this.eventBus = eventBus;
+    this.container = createElement("aside", { classes: ["project__sidebar"] });
+  }
+
+  render(projects) {
+    this.container.replaceChildren(
+      this.renderList(projects),
+      this.renderButton(),
+    );
+  }
+
+  renderList(projects) {
+    return createElement(
+      "ul",
+      { classes: ["project__list"] },
+      ...projects.map((project) =>
+        createElement("li", { classes: ["project__item"] }, project.name),
+      ),
+    );
+  }
+
+  renderButton() {
+    return createElement(
+      "button",
+      {
+        classes: ["project__button"],
+        onClick: () => {
+          const name = prompt("Project name");
+          if (!name?.trim()) return;
+          this.eventBus.publish(EVENTS.UI.ADD_PROJECT_CLICKED, name.trim());
+        },
+      },
+      "Add Project",
+    );
+  }
+
+  get element() {
+    return this.container;
+  }
+}
