2976d41b28fde7feaa5ade9085ce81f95bdc885a
cal <sakphea05@gmail.com>
2026-08-16 21:48:32 +0700

    feat: wire up app entry point

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+import { ProjectList } from "./components/ProjectList.js";
+import { EVENTS } from "./constants/events.js";
+import { ProjectController } from "./controllers/ProjectController.js";
+import { Project } from "./models/Project.js";
+import { ProjectStore } from "./store/store.js";
+import { EventEmitter } from "./utils/EventEmitter.js";
+
+import "./styles/reset.css";
+
+const container = document.querySelector("#content");
+const eventBus = new EventEmitter();
+const store = new ProjectStore(eventBus);
+const projectList = new ProjectList(eventBus);
+
+new ProjectController(eventBus, store, projectList, container);
+
+store.addProject("Inbox");
